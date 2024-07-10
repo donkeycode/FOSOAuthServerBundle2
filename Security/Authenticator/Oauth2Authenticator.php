cf75f2b9e927c2b3698b151adf5a0c972a5b413b
Cédric LOMBARDOT <cedric.lombardot@gmail.com>
2024-07-10 20:28:21 +0200

    Fix return identifier

--- Security/Authenticator/Oauth2Authenticator.php
+++ Security/Authenticator/Oauth2Authenticator.php
@@ -90,7 +90,7 @@ class Oauth2Authenticator extends AbstractAuthenticator
             $roles = array_unique($roles, SORT_REGULAR);
             $accessTokenBadge = new AccessTokenBadge( $accessToken, $roles );
 
-            return new SelfValidatingPassport( new UserBadge( $client->getUserIdentifier() ), [ $accessTokenBadge ] );
+            return new SelfValidatingPassport( new UserBadge( $user->getUserIdentifier() ), [ $accessTokenBadge ] );
         } catch (OAuth2ServerException $e) {
             throw new AuthenticationException('OAuth2 authentication failed', 0, $e);
         }
